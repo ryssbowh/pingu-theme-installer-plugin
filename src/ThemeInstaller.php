b69803d4c7bc1060cedeca577ee8954884d1ce6d
Unknown <blondin.boris@gmail.com>
2019-05-03 20:37:04 +0100

    first commit

---
+++ src/ThemeInstaller.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace ThemeInstaller;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class ThemeInstaller extends LibraryInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        preg_match('/^pingu\/([a-z]+)-theme$/', $package->getPrettyName(), $matches);
+        if (sizeof($matches) != 2) {
+            throw new \InvalidArgumentException(
+                'Unable to install theme, pingu theme must have the following name : pingu/{name}-theme';
+            );
+        }
+
+        return 'public/themes/'.ucfirst($matches[1]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return 'pingu-theme' === $packageType;
+    }
+}
---
+++ src/ThemeInstallerPlugin.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace ThemeInstaller;
+
+use Composer\Composer;
+use Composer\Plugin\PluginInterface;
+
+class ThemeInstallerPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new ThemeInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
